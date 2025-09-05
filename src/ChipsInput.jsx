955c13f88a56b9a528412e1c2e26eb0ce584b1f7
Niraj1903 <niraj.singh1903@outlook.com>
2025-09-05 23:28:05 +0530

    input text value binded using useState

--- src/ChipsInput.jsx
+++ src/ChipsInput.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ChipsInput = () => {
-  return <div>ChipsInput</div>;
+  const [input, setInput] = useState("");
+  return (
+    <>
+      <div className="flex">
+        <h1 className="font-bold text-black">Chips-Input</h1>
+        <input
+          className="border border-black"
+          type="text"
+          value={input}
+          placeholder="Type chips or press enter"
+          onChange={(e) => setInput(e.target.value)}
+        />
+      </div>
+    </>
+  );
 };
 
 export default ChipsInput;
