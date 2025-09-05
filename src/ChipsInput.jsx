c0add0a8a5cda4438cd256374766d306caa89830
Niraj1903 <niraj.singh1903@outlook.com>
2025-09-05 23:40:44 +0530

    onKeyDown === Enter logic created

--- src/ChipsInput.jsx
+++ src/ChipsInput.jsx
@@ -2,6 +2,14 @@ import React, { useState } from "react";
 
 const ChipsInput = () => {
   const [input, setInput] = useState("");
+  const [chips, setChips] = useState([]);
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      setChips((prev) => [...prev, input]);
+      setInput("");
+    }
+  };
   return (
     <>
       <div className="flex">
@@ -12,7 +20,14 @@ const ChipsInput = () => {
           value={input}
           placeholder="Type chips or press enter"
           onChange={(e) => setInput(e.target.value)}
+          onKeyDown={(e) => handleKeyDown(e)}
         />
+      </div>
+
+      <div>
+        {chips.map((item) => (
+          <h6 key={item}>{item}</h6>
+        ))}
       </div>
     </>
   );
